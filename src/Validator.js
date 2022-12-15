37b17d10728cbea754b8c8f09074992ffe8f7ad2
Pontus Fagerström <pontus.fagerstrom@fripost.org>
2022-12-15 20:23:30 +0100

    fix: add validation of wikipage object type

--- src/Validator.js
+++ src/Validator.js
@@ -1,3 +1,9 @@
+import WikiPage from "./WikiPage";
+
 export const isString = (input) => {
   return typeof input == "string";
+};
+
+export const isWikiPage = (obj) => {
+  return obj instanceof WikiPage;
 };
