f435c97df4daaf056eaf2750212fc4614280277d
Pontus Fagerström <pontus.fagerstrom@fripost.org>
2022-12-18 00:27:52 +0100

    add ParentId class

--- src/ParentId.js
+++ src/ParentId.js
@@ -1,9 +1,13 @@
+import { isNonEmptyString } from "./Validator.js";
+
 export default class ParentId {
   courseId;
   groupId;
 
   constructor(courseId, groupId) {
-    // TODO: add validation and encapsulate members
+    if (!isNonEmptyString(courseId) && !isNonEmptyString(groupId)) {
+      throw `${this.constructor.name}: eiter courseId or groupId must be present`;
+    }
     this.courseId = courseId;
     this.groupId = groupId;
   }
